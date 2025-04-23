196dd2cd0cc7cc7814b4684702772214b377aefb
KarthikNarsapur <karthik.n@cloudthat.com>
2025-04-23 17:02:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "aws_kms_key" "kms_key" {
 
 
 resource "aws_kms_alias" "kms-alias" {
-  name          = "alias/${var.key_alias}"
+  name          = "alias/${var.key-alias}"
   target_key_id = aws_kms_key.kms_key.key_id
 }
