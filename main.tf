21225d7fe1221e1550361602a70afb505a2150c2
KarthikNarsapur <karthik.n@cloudthat.com>
2025-04-23 16:21:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,35 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+resource "aws_kms_key" "kms_key" {
+  description         = var.description
+  enable_key_rotation = true
 }
 
-terraform {
-  backend "s3" {
-    bucket = "interns-backstage.io-statefile"
-    key    = "project/terraform.tfstate"
-    region = "ap-south-1"
-    encrypt = true
-  }
+resource "aws_kms_alias" "kms_alias" {
+  name          = var.key_alias
+  target_key_id = aws_kms_key.kms_key.key_id
 }
- 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  force_destroy = true
-  tags = {
-    CreatedBy = "Backstage"
-  }
-}
-
-resource "aws_s3_bucket_versioning" "versioning" {
-  count  = var.versioning ? 1 : 0
-  bucket = aws_s3_bucket.my_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
- 
