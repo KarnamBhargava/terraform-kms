e5c5fe6f640e52874025f78b43c85d0f391eace3
KarnamBhargava <karnam.b@cloudthat.com>
2025-04-28 10:22:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 terraform {
   backend "s3" {
-    bucket = "interns-backstage.io-statefile"
+    bucket = "backstage-interns-project"
     key    = "project/terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
   }
 }
-
 
 
 resource "aws_kms_key" "kms_key" {
